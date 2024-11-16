549a3bbea7d44a3845ffbd1bb832ad58ee4d8c42
vgewilliam <vgewilliam@163.com>
2024-11-16 19:32:48 +0100

    Change default theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -90,7 +90,8 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+    if (userPref && (userPref('(prefers-color-scheme: dark)').matches || userPref('(max-width: 768px)').matches || true)) {
+    // if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
       theme = "dark";
     }
   }
